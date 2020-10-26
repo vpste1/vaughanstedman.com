a4ea2a2664d25ee43b0e83cc0b7322ec5416e082
Vaughan Stedman <vaughan.stedman@gmail.com>
2020-10-26 11:54:21 +1100

    chore: update plugin to support https querying

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ module.exports = {
       resolve: `gatsby-plugin-s3`,
       options: {
         bucketName: "vaughanstedman.com",
+        protocol: "https",
+        hostname: "www.vaughanstedman.com",
       },
     },
     `gatsby-plugin-react-helmet`,
